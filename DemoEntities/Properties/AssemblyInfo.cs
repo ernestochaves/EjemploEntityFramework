78e62e082e573c87923edccbaf60b5abd30c7b94
Ernesto Chaves Chaves <ernestochaveschaves@outlook.com>
2018-07-20 15:35:35 -0600

    First project setup

--- DemoEntities/Properties/AssemblyInfo.cs
+++ DemoEntities/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DemoEntities")]
+[assembly: AssemblyTitle("Demo.Entities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DemoEntities")]
+[assembly: AssemblyProduct("Demo.Entities")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
